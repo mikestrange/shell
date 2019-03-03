49221df8b5238d0813d1fbeaacad3ccf8eeb114b
Mike <xinyue___ld@sina.cn>
2019-03-03 14:23:33 +0800

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -39,7 +39,7 @@ yum install screen
 yum install php php-devel
 #php扩展
 yum install php-mysql php-gd php-ldap php-odbc php-pear php-xml php-xmlrpc
-###=================php升级到5.6 默认5.4=================
+###=================php升级到5.6 默认5.4(centos 7.0+)=================
 rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm  
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 #删除现在的php版本
@@ -48,6 +48,8 @@ yum remove php-common
 yum install -y php56w php56w-opcache php56w-xml php56w-mcrypt php56w-gd php56w-mysql php56w-intl php56w-mbstring 
 #查看新的php版本
 php -v
+#最后记得重启httpd
+systemctl restart httpd
 ###=================php升级到5.6 默认5.4=================
 
 yum list installed | grep mysql
